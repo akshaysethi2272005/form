b4d120e3d58f6c1bc683e5ce7871675dcd68d3bc
samanyu103 <mahajansamanyu@gmail.com>
2024-05-23 20:37:59 +0530

    using script.js

--- index.js
+++ index.js
@@ -15,22 +15,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-    res.render('index',{message:null})
+    res.render('index')
 })
 
 app.post('/',(req , res) => {
     const username = req.body.username;
     const password = req.body.password;
     const body = req.body;
-    // if (username.trim() == '' || password.trim() == ''){
-    //     console.log("alert sent");
-    //     res.render('index',{message:"alert"});    
-    // }else{
+    if (username.trim() == '' || password.trim() == ''){
+        console.log("alert sent");
+        // res.render('index');
+        return;    
+    }else{
         console.log("username "+username.toString()+"\n" +"password "+password.toString());
         // res.render('index',{message:null});
 
         res.render('response',{user:username,pass:password});
-    // }
+    }
 })
   
 app.listen(port , () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,11 @@
 document.getElementById("btn").addEventListener('click',() => {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
     if (username=='' || password == '') {
         alert("empty field");
+        window.location.replace(window.location.href);
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
     }
 });
 
