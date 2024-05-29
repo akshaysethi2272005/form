36b40c23903b2a79685d36dc52db7321f25cd4b6
akshaysethi2272005 <sethisadhana26@gmail.com>
2024-05-29 10:05:17 +0530

    world

--- index.js
+++ index.js
@@ -4,12 +4,40 @@ const app = express();
 const port = 3000
 const path = require('path');
 const admin = require('firebase-admin');
-var serviceAccount = require("./auth-sam-c8614-firebase-adminsdk-so9lh-59c590b9ee.json");
+var serviceAccount = require("./auth-samm-firebase-adminsdk-kk62o-11f2d1b2f4.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+const firebaseConfig = {
+    apiKey: "AIzaSyA7LOTC_1lOCqFRmpAlAYGcQzEj2M1Dq-w",
+    authDomain: "auth-samm.firebaseapp.com",
+    projectId: "auth-samm",
+    storageBucket: "auth-samm.appspot.com",
+    messagingSenderId: "722138859828",
+    appId: "1:722138859828:web:7ed491d1cba38666bebd61",
+    measurementId: "G-V7MPMZYV8Y"
+};
+const firebase = require('firebase/app');
+firebase.initializeApp(firebase);
 const auth = admin.auth()
 const firestore = admin.firestore();
+const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for
+    // this URL must be whitelisted in the Firebase Console.
+    url: 'localhost:3000/',
+    // This must be true for email link sign-in.
+    handleCodeInApp: true,
+    iOS: {
+      bundleId: 'com.example.ios',
+    },
+    android: {
+      packageName: 'com.example.android',
+      installApp: true,
+      minimumVersion: '12',
+    },
+    // FDL custom domain.
+    dynamicLinkDomain: 'coolapp.page.link',
+  };
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -44,9 +72,22 @@ app.post('/',(req , res) => {
                 } 
             })
             if (ok) {
-                console.log("logged in");
-                // res.send("logged in");
-                res.render("response",{user:username,pass:password});
+                auth.getUserByEmail(cred.username).then(val => {
+                    console.log(val)
+                    console.log("logged in");
+                    // res.send("logged in");
+                    if(val.emailVerified === true){
+                        res.render("response",{user:username,pass:password,ev:val.emailVerified, link:null});
+                    }else{
+                        auth.generateEmailVerificationLink(cred.username,actionCodeSettings).then(li => {
+                            res.render("response",{user:username,pass:password,ev:val.emailVerified, link:li});
+                        }).catch((err) => {
+                            console.log(err)
+                        })
+                    }
+                }).catch(err => {
+                    console.log(err)
+                })
             }else {
                 res.render('index',{ale:"incorrect password"});
             }
@@ -70,7 +111,6 @@ app.post('/',(req , res) => {
         // })
     }
 })
-  
 app.get("/create",(req,res)=> {
     res.render('create')
 })
@@ -97,7 +137,8 @@ app.post('/create',(req , res) => {
             try{
                 firestore.collection('users').add({
                     username:username,
-                    password:password
+                    password:password,
+                    emailverified:false
                     // hash:user.passwordHash,
                     // salt:user.passwordSalt
                 })
